c922fdce7d29b7bf3f63fe8b5ded246317844ff1
Josej36Aguirre <josej36.aguirre@gmail.com>
2018-05-29 21:21:20 -0500

    werghj

--- Clima/Clima/ViewModel/WeatherViewModelPage.cs
+++ Clima/Clima/ViewModel/WeatherViewModelPage.cs
@@ -22,94 +22,131 @@ namespace Clima.ViewModel
         private ImageSource imagen;
         #endregion
 
-      
 
-        public string Ubicacion
+
+        #region Propiedades
+
+        public ImageSource Imagen
         {
-            get { return ubicacion; }
-            set { ubicacion = value; }
+            get { return imagen; }
+            set
+            {
+                SetValue(ref imagen, value);
+            }
         }
-      
 
-        public string Pais
+
+        public string Temperatura
         {
-            get { return pais; }
-            set { pais = value; }
+            get { return temperatura; }
+            set
+            {
+                SetValue(ref temperatura, value);
+            }
+        }
+
+
+        public string Clima
+        {
+            get { return clima; }
+            set
+            {
+                SetValue(ref clima, value);
+            }
+        }
+
+
+        public string UltimaActualizacion
+        {
+            get { return ultimaActualizacion; }
+            set
+            {
+                SetValue(ref ultimaActualizacion, value);
+            }
+        }
+
+
+        public string Region
+        {
+            get { return region; }
+            set
+            {
+                SetValue(ref region, value);
+            }
         }
 
 
         public string ResultTerm
         {
             get { return resultTerm; }
-            set { resultTerm = value; }
-        }
-
-       
-
-        public string Region
-        {
-            get { return region; }
-            set { region = value; }
-        }
-      
-
-        public string UltimaActualizacion
-        {
-            get { return ultimaActualizacion; }
-            set { ultimaActualizacion = value; }
-        }
-
-     
-
-        public string Clima
-        {
-            get { return clima; }
-            set { clima = value; }
-        }
-        
-        public string Temperatura
-        {
-            get { return temperatura; }
-            set { temperatura = value; }
-        }
-
-
-        public ImageSource Imagen
-        {
-            get { return imagen; }
-            set { imagen = value; }
-        }
-
-        public ICommand BuscarComand()
-        {
-            get{
-                return new RelayComand(Buscar);
+            set
+            {
+                SetValue(ref resultTerm, value);
             }
-
         }
-        public async void Buscar()
+
+
+        public string Pais
+        {
+            get { return pais; }
+            set
+            {
+                SetValue(ref pais, value);
+            }
+        }
+
+
+        public string Ubicacion
+        {
+            get { return ubicacion; }
+            set
+            {
+                SetValue(ref ubicacion, value);
+            }
+        }
+
+
+        #endregion
+
+        #region Commandos
+
+        public ICommand BuscarCommand
+        {
+            get
+            {
+                return new RelayCommand(Buscar);
+            }
+        }
+        #endregion
+
+        #region Metodos
+        private async void Buscar()
         {
             HttpClient cliente = new HttpClient();
             cliente.BaseAddress = new Uri(ObtenerURL());
             var response = await cliente.GetAsync(cliente.BaseAddress);
             response.EnsureSuccessStatusCode();
-            var jsonResult = response.Content.ReadAsByteArrayAsync().Result;
-            var weatherModel = Weather. .Model.FromJson(jsonResult);
+            var jsonResult = response.Content.ReadAsStringAsync().Result;
+            var weatherModel = Weather.FromJson(jsonResult);
             FijarValores(weatherModel);
         }
-        public string ObtenerURL()
+
+        private void FijarValores(Weather weatherModel)
         {
-            string serviceURL = $"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Tunja%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+        }
+
+        private string ObtenerURL()
+        {
+            string serviceURL = $"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{ResultTerm}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
             return serviceURL;
         }
-        public void FijarValores(Weather weatherModel)
-        {
+        #endregion
 
 
-        }
-        
 
-        
+
+
 
     }
 }
